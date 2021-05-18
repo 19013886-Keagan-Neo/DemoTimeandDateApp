cce922a0f45aa41705b7367837bb695cd0581a82
19013886 <19013886@myrp.edu.sg>
2021-05-18 11:38:40 +0800

    Date/Time Picker done

---
+++ app/src/main/java/sg/edu/rp/c346/id19013886/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,80 @@
+package sg.edu.rp.c346.id19013886.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Step 1: Declare the field variables
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    Button btnReset;
+    TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Step 2: Link the field variable to UI components in Layout
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        btnReset = findViewById(R.id.buttonReset);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Code for the action
+                String msg = "Time is ";
+                int hour = tp.getCurrentHour();
+                int min = tp.getCurrentMinute();
+
+                if (min < 11){
+                    msg = msg + hour + ":0" + min;
+                } else {
+                    msg = msg + hour + ":" + min;
+                }
+
+                tvDisplay.setText(msg);
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Code for the action
+                String msg = "Date is ";
+                int day = dp.getDayOfMonth();
+                int month = dp.getMonth() + 1;
+                int year = dp.getYear();
+                msg = msg + day + "/" + month + "/" + year;
+                tvDisplay.setText(msg);
+
+            }
+        });
+
+        // Enhancement - Reset Button
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2020, 0, 1); // Month is zero-based
+            }
+        });
+
+    }
+}
